5660b29743b65054b14dc9909010452e751f6a1a
Jemy <jemygraw@gmail.com>
2016-09-30 12:14:01 +0800

    update build script

--- src/cross_build.sh
+++ src/cross_build.sh
@@ -1,8 +1,8 @@
 export GOPATH=$GOPATH:/Users/jemy/QiniuCloud/Projects/qfetch
 GOOS="darwin" GOARCH="amd64" go build -o "../bin/qfetch_darwin_amd64" main.go 
 GOOS="darwin" GOARCH="386" go build -o "../bin/qfetch_darwin_386" main.go
-GOOS="windows" GOARCH="amd64" go build -o "../bin/qfetch_windows_amd64" main.go
-GOOS="windows" GOARCH="386" go build -o "../bin/qfetch_windows_386" main.go
+GOOS="windows" GOARCH="amd64" go build -o "../bin/qfetch_windows_amd64.exe" main.go
+GOOS="windows" GOARCH="386" go build -o "../bin/qfetch_windows_386.exe" main.go
 GOOS="linux" GOARCH="amd64" go build -o "../bin/qfetch_linux_amd64" main.go
 GOOS="linux" GOARCH="386" go build -o "../bin/qfetch_linux_386" main.go
 GOOS="linux" GOARCH="arm" go build -o "../bin/qfetch_linux_arm" main.go
